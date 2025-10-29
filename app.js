3d6199fc24a2bba06ccc2474fb3ca9e857dd57cc
Eliav Shames <101091197+eliavs-snyk@users.noreply.github.com>
2025-10-29 12:13:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var app = express();
 var routes = require('./routes');
 var routesUsers = require('./routes/users.js')
 
-// all environments
+// all environmentsgggg
 app.set('port', process.env.PORT || 3001);
 app.engine('ejs', ejsEngine);
 app.engine('dust', cons.dust);
