70a9ae596c780b566ec8639dd2e7a7ee750cb1b7
Eliav Shames <101091197+eliavs-snyk@users.noreply.github.com>
2026-02-15 13:55:27 +0200

    Add secret token variable to index.js

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var readline = require('readline');
 var moment = require('moment');
 var exec = require('child_process').exec;
 var validator = require('validator');
-
+var secret_token = '1234_TOKEN_ABCDE';
 // zip-slip
 var fileType = require('file-type');
 var AdmZip = require('adm-zip');
